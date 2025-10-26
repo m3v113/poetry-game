35ea4ae0e37dc1d9600a44db45a3a6c54d681010
= <=>
2025-10-25 19:11:35 -0700

    Everything

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,18 +1,36 @@
 import { useEffect, useState } from 'react'
-//import { onAuthStateChanged, signOut } from 'firebase/auth';
-//import { auth } from 'firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from './firebase';
 import './App.css'
+import Login from './components/login.jsx';
+import FridgeMagnetPoetry from './components/FridgeMagnetPoetry.jsx';
 
 function App() {
-  
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  <h1>Welcome to </h1>
+
+  if (!user) return <Login onLogin={() => {}} />;
 
   return (
-    <>
-      
-    </>
-
-  )
+    <div class = "App">
+      <h1>Welcome, {user.email}</h1>
+      <button
+        onClick={() => signOut(auth)}
+      >
+        Logout
+      </button>
+      <FridgeMagnetPoetry/>
+    </div>
+  );
 }
 
 export default App
